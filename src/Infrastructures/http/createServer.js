2a89fcb71bc435f5625d66fbeee8a2fc78bc8c67
dhamaddam <dhamaddam@gmail.com>
2022-07-29 18:22:23 +0700

    add say hello world test case

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -61,7 +61,7 @@ const createServer = async(container) => {
         method: 'GET',
         path: '/',
         handler: () => ({
-            value: 'Hello world!',
+            value: 'Hello world Hai Dicoding!',
         }),
     });
 
